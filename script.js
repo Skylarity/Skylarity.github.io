1908b803564529e0309e6a21a8f831e2078fab4e
Skyler Rexroad <skyrex1095@gmail.com>
2014-09-23 09:46:41 -0600

    First Commit!
    Website 1.0!

---
+++ script.js
@@ -0,0 +1,25 @@
+function leftImg() {
+	var image = document.getElementById("imagedisplayed").src;
+	var imgchar = image.charAt(image.length - 5);
+	var imgnum = parseInt(imgchar);
+
+	imgnum--;
+	if (imgnum < 1) {
+		imgnum = 7;
+	}
+
+	document.getElementById("imagedisplayed").src = "images/gallery/" + imgnum + ".png";
+}
+
+function rightImg() {
+	var image = document.getElementById("imagedisplayed").src;
+	var imgchar = image.charAt(image.length - 5);
+	var imgnum = parseInt(imgchar);
+
+	imgnum++;
+	if (imgnum > 7) {
+		imgnum = 1;
+	}
+
+	document.getElementById("imagedisplayed").src = "images/gallery/" + imgnum + ".png";
+}
